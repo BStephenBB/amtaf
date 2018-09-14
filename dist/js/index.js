623eed3f1a7f982e017df1e84501e7ea21a703b3
Stephen Brown-Bourne <sbrownbourne@gmail.com>
2018-09-14 06:02:24 -0700

    created temp dist folder and added gh pages

---
+++ dist/js/index.js
@@ -0,0 +1,11 @@
+// console.log("Hello World");
+
+// let tagContainers = document.getElementsByClassName("the-tags");
+// console.log(tagContainers);
+// let innTags = tagContainers[0].getElementsByClassName("tag");
+// console.log(innTags);
+// console.log(innTags[0]);
+// innTags[1].style.right = "35px";
+
+// let tagColors = document.querySelector(".innerCircle");
+// tagColors.style.backgroundColor = "lightgreen";
